00a6585ad66b5d19cbcd7af863bf0f1325f772a5
Mariano-RA <rodriguezarce.mariano@gmail.com>
2022-11-18 11:26:05 -0300

    Se crean carpetas para hooks custom

---
+++ 06-mis-custom-hooks/useForm/useForm.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const useForm = (initialValue = {}) => {
+  const [formState, setFormState] = useState(initialValue);
+
+  // 
+
+  const onResetForm = () => {
+    setFormState(initialValue);
+  };
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm
+  };
+};
